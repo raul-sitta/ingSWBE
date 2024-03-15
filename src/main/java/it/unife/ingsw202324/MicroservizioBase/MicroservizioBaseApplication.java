a1dff91a7ba5403495e1d229e3cb5baec07c72d2
Raul <raul.sitta@edu.unife.it>
2024-03-15 16:50:02 +0100

    First commit

---
+++ src/main/java/it/unife/ingsw202324/MicroservizioBase/MicroservizioBaseApplication.java
@@ -0,0 +1,13 @@
+package it.unife.ingsw202324.MicroservizioBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservizioBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservizioBaseApplication.class, args);
+	}
+
+}
